ce8a5d693c5d2e0ec8cd706e7b2a6fe8ba34ce49
sendm3 <sendm3@mst.edu>
2019-02-13 15:21:00 -0600

    more changes and bugfixes to the raid gun

--- Source/UltimateWeaponsMod/UltimateWeaponsMod/Projectile_RaidBullet.cs
+++ Source/UltimateWeaponsMod/UltimateWeaponsMod/Projectile_RaidBullet.cs
@@ -35,31 +35,32 @@ namespace UltimateWeaponsMod
                 //DebugOutputsIncidents.RaidFactionSampled();
                 //DebugOutputsIncidents.RaidStrategySampled();
             Random rand111 = new Random();
-            Log.Message("line 1", true);
+            //Log.Message("line 1", true);
             int randomNumber = rand111.Next(50, 1000);
             randomNumber = (int)Math.Round((randomNumber * Def.raidSeverityMultiplier));
-            Log.Message("points = " + randomNumber, true);
+            //Log.Message("points = " + randomNumber, true);
             IncidentParms ind222 = new IncidentParms();
-            Log.Message("line 2", true);
+            //Log.Message("line 2", true);
             ind222.points = randomNumber;
             ind222.raidArrivalMode = PawnsArrivalModeDefOf.EdgeWalkIn;
-            Log.Message("line 3", true);
+            //Log.Message("line 3", true);
             ind222.raidStrategy = RaidStrategyDefOf.ImmediateAttack;
-            Log.Message("line 4", true);
+            //Log.Message("line 4", true);
             ind222.faction = Find.FactionManager.RandomEnemyFaction();
             ind222.pawnGroupMakerSeed = 122;
-            Log.Message("line 5", true);
+            //Log.Message("line 5", true);
             ind222.target = hitThing.Map;
-            Log.Message("this map to string = " + hitThing.Map.ToString(), true);
-            Log.Message("line 6", true);
+            //Log.Message("this map to string = " + hitThing.Map.ToString(), true);
+            //Log.Message("line 6", true);
             //IncidentDef indicent1 = IncidentDefOf.RaidEnemy;
-            Log.Message("line 7", true);
+            //Log.Message("line 7", true);
             //ind222.ExposeData();
             ind222.forced = true;
             IncidentDef indicent1 = (!ind222.faction.HostileTo(Faction.OfPlayer)) ? IncidentDefOf.RaidFriendly : IncidentDefOf.RaidEnemy;
             indicent1.baseChance = 1.0f;
             indicent1.Worker.TryExecute(ind222);
-            Log.Message("done", true);
+            //Log.Message("done", true);
+            //well that was a pain in the ass.
             //}
         }
         #endregion Overrides
